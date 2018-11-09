26d056a937d56a70558c4b9e49a1938924f33e23
Tchuuii <c.lacroix-durant@hotmail.fr>
2018-11-09 16:17:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import time,random
-
-
+import epd1in54b
+import Image
+import ImageFont
+import ImageDraw
 from gpiozero import Button,LED
+
+
 button1 = Button(24)
 button2 = Button(12)
 button3 = Button(18)
 led = LED(23)
 
+COLORED = 1
+UNCOLORED = 0
 
 class Tamagotchi: 
     def __init__(self,name):
@@ -23,7 +29,22 @@ class Tamagotchi:
     
 if __name__ == '__main__':
     
-    print('Welcome to Tamapatate')
+    epd = epd1in54b.EPD()
+    epd.init()
+    # clear the frame buffer
+    frame_black = [0xFF] * (epd.width * epd.height / 8)
+    frame_red = [0xFF] * (epd.width * epd.height / 8)
+    
+    # draw background
+    epd.draw_filled_rectangle(frame_black, 30, 55, 170, 80, COLORED);
+    
+    # write strings to the buffer
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 18)
+    epd.display_string_at(frame_black, 30, 30, "Welcome to", font, COLORED)
+    epd.display_string_at(frame_red, 30, 60, "TAMAPATATE", font, UNCOLORED)
+    epd.display_frame(frame_black, frame_red)
+    epd.sleep()
+    
     tamapatate = Tamagotchi("patate")
     alert = False
     print('Press Ctrl+C to exit')
@@ -31,9 +52,6 @@ if __name__ == '__main__':
     try:
         while(True):
              
-            if(button1.ispressed()):
-                print('menu')
-                
             statMiam=random.randint(0,100)
             #print('miam')
             #print(statMiam)
@@ -49,14 +67,19 @@ if __name__ == '__main__':
                 tamapatate.joy-=1
             if(statPoo>=97) and (tamapatate.poo>0):
                 tamapatate.poo-=1
-                print('poopoo')
-                
+                print('poopoo')    
+                epd.display_string_at(frame_black, 70, 150, "popo", font, COLORED)
+                epd.display_frame(frame_black, frame_red)
+                epd.sleep()
+               
             if((tamapatate.miam==0) or (tamapatate.joy==0) or (tamapatate.poo<2)) and alert==False:
                 alert=True
-
+                epd.display_string_at(frame_black, 45, 120, "ALERT", font, UNCOLORED)
+                epd.draw_filled_rectangle(frame_red, 30, 115, 170, 140, COLORED)
+                epd.display_frame(frame_black, frame_red)
+                epd.sleep()
             
             time.sleep(0.2)
     except KeyboardInterrupt:
         time.sleep(1)
         raise
-
