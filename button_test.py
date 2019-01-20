b709f806cecef43bf682663b9eed151c65ef8464
Patate <patate@Patate.local>
2019-01-20 22:57:31 +0100

    minor fix

--- button_test.py
+++ button_test.py
@@ -20,7 +20,7 @@ GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 12 to be an input 
 GPIO.add_event_detect(12,GPIO.RISING,callback=button2_callback) # Setup event on pin 12 rising edge
 
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 16 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(16,GPIO.RISING,callback=button2_callback) # Setup event on pin 16 rising edge
+GPIO.add_event_detect(16,GPIO.RISING,callback=button3_callback) # Setup event on pin 16 rising edge
 
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
