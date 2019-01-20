0a91a79aca45ca0569195ce144af56af65eab1b1
Patate <patate@Patate.local>
2019-01-20 22:22:40 +0100

    test

--- button_test.py
+++ button_test.py
@@ -1,1 +1,10 @@
-# yolo 
+# yolo
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True: # Run forever
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pushed!")
