8400d13eed5f3ed38fd9d61970f0a142c1faa5c1
Patate <patate@Patate.local>
2019-01-21 20:43:07 +0100

    test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time,random
 import RPi.GPIO as GPIO
+
+import time,random
+
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
@@ -10,19 +12,22 @@ from PIL import ImageFont
 A_pin = 10
 B_pin = 12
 C_pin = 16
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(L_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(R_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(C_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+
 # Raspberry Pi pin configuration:
 RST = 24
 # Note the following are only used with SPI:
 DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
-mlevel = 0 #main level
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(A_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(B_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 12 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(C_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 16 to be an input pin and set initial value to be pulled low (off)
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+mlevel = 0 #main level
 
 class Tamagotchi:
     def __init__(self,name):
@@ -72,41 +77,25 @@ def button3_callback(channel):
 
 
 if __name__ == '__main__':
-
-
-    tamapatate = Tamagotchi("patate")
-    #------------DISPLAY SETTING ---------------
-    # 128x32 display with hardware I2C:
-    disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
     # Initialize library.
     disp.begin()
+
     # Clear display.
     disp.clear()
     disp.display()
-    # Clear display.
-    disp.clear()
-    disp.display()
-    # Create blank image for drawing.
-    # Make sure to create image with mode '1' for 1-bit color.
     width = disp.width
     height = disp.height
     image = Image.new('1', (width, height))
     # Get drawing object to draw on image.
     draw = ImageDraw.Draw(image)
+
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
-    # Draw some shapes.
-    # First define some constants to allow easy resizing of shapes.
-    padding = 2
-    shape_width = 20
-    top = padding
-    bottom = height-padding
-    # Move left to right keeping track of the current x position for drawing shapes.
-    x = padding
-    # Load default font.
-    font = ImageFont.load_default()
+
+    tamapatate = Tamagotchi("patate")
+
     #------------BUTTONS SETTING ---------------
-    
+
     GPIO.add_event_detect(A_pin,GPIO.RISING,callback=button1_callback) # Setup event on pin 10 rising edge
     GPIO.add_event_detect(B_pin,GPIO.RISING,callback=button2_callback) # Setup event on pin 12 rising edge
     GPIO.add_event_detect(C_pin,GPIO.RISING,callback=button3_callback) # Setup event on pin 16 rising edge
