d2622c5e61ff14b6f6a8156950b186ea8cd71dc7
Patate <patate@Patate.local>
2019-01-22 14:56:54 +0100

    test

--- main.py
+++ main.py
@@ -93,9 +93,9 @@ if __name__ == '__main__':
 
     #------------BUTTONS SETTING ---------------
 
-    GPIO.add_event_detect(A_pin,GPIO.RISING,callback=tamapatate.button1_callback()) # Setup event on pin 10 rising edge
-    GPIO.add_event_detect(B_pin,GPIO.RISING,callback=tamapatate.button2_callback()) # Setup event on pin 12 rising edge
-    GPIO.add_event_detect(C_pin,GPIO.RISING,callback=tamapatate.button3_callback()) # Setup event on pin 16 rising edge
+    GPIO.add_event_detect(A_pin,GPIO.RISING,callback=tamapatate.button1_callback(tamapatate)) # Setup event on pin 10 rising edge
+    GPIO.add_event_detect(B_pin,GPIO.RISING,callback=tamapatate.button2_callback(tamapatate)) # Setup event on pin 12 rising edge
+    GPIO.add_event_detect(C_pin,GPIO.RISING,callback=tamapatate.button3_callback(tamapatate)) # Setup event on pin 16 rising edge
 
 
     alert = False
