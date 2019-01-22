c1fcca17d9801bfb8d2209cbc6de4e08724c838c
Patate <patate@Patate.local>
2019-01-22 18:29:14 +0100

    new screen test from scratch

--- screen_test.py
+++ screen_test.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 
-import time
+import time,random
 
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
@@ -9,18 +9,15 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-# Input pins:
-L_pin = 10
-R_pin = 12
-C_pin = 16
+A_pin = 26
+B_pin = 6
+C_pin = 5
 
+GPIO.setmode(GPIO.BCM) # Use BCM pin numbering
 
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(L_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
-GPIO.setup(R_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(A_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(B_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
 GPIO.setup(C_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
-
 
 # Raspberry Pi pin configuration:
 RST = 24
@@ -29,71 +26,60 @@ DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
 
-# Beaglebone Black pin configuration:
-# RST = 'P9_12'
-# Note the following are only used with SPI:
-# DC = 'P9_15'
-# SPI_PORT = 1
-# SPI_DEVICE = 0
-
-# 128x32 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-
-# 128x64 display with hardware I2C:
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
-# Note you can change the I2C address by passing an i2c_address parameter like:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+mlevel = 0 #main level
 
-# Initialize library.
+def button1_callback(channel):
+    global mlevel
+    print("Button 1 was pushed!")
+    if mlevel<=10:
+        mlevel+=1
+    else:
+        mlevel=0
+
+def button2_callback(channel):
+    global mlevel
+    print("Button 2 was pushed!")
+    if mlevel==1:
+        print("menu1")
+    elif mlevel==2:
+        print("menu2")
+    elif mlevel==3:
+        print("menu3")
+
+def button3_callback(channel):
+    global mlevel
+    print("Button 3 was pushed!")
+    mlevel=0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+GPIO.add_event_detect(26,GPIO.RISING,callback=button1_callback)
+GPIO.add_event_detect(6,GPIO.RISING,callback=button2_callback)
+GPIO.add_event_detect(5,GPIO.RISING,callback=button3_callback)
 disp.begin()
 
 # Clear display.
 disp.clear()
 disp.display()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
-
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
 
 # Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
+font = ImageFont.load_default()
 
+message = input("Press enter to quit\n\n") # Run until someone presses enter
 
-try:
-    while 1:
-
-        if GPIO.input(L_pin): # button is released
-            draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0)  #left
-        else: # button is pressed:
-            draw.text((2, top+20), 'World!', font=font, fill=255)
-        if GPIO.input(R_pin): # button is released
-            draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0) #right
-        else: # button is pressed:
-            draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=1) #right filled
-
-
-        if GPIO.input(C_pin): # button is released
-            draw.rectangle((20, 22,40,40), outline=255, fill=0) #center
-        else: # button is pressed:
-            draw.rectangle((20, 22,40,40), outline=255, fill=1) #center filled
-
-
-        if not GPIO.input(C_pin) and not GPIO.input(BR_pin) and not GPIO.input(L_pin):
-            catImage = Image.open('happycat_oled_64.ppm').convert('1')
-            disp.image(catImage)
-        else:
-            # Display image.
-            disp.image(image)
-
-        disp.display()
-        time.sleep(.01)
-
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+draw.text((2, 2),    'Hello',  font=font, fill=255)
+draw.text((2, 22), 'tamapatate', font=font, fill=255)
+# Display image.
+disp.image(image)
+disp.display()
+GPIO.cleanup() # Clean up
